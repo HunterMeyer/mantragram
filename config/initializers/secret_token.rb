b1a6ef7b4d2ed4db9c262040d4f011b54ba766da
Hunter Meyer <huntermeyer@live.com>
2014-10-03 21:07:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mantragram::Application.config.secret_token = 'd09ab6ba584995d1da11249875f310cc38b516b979e1279c29212714d25038d60c95bcb7112791712bc5fc22cde63e9e01c88c37e71a580f57511b5ac485b620'
