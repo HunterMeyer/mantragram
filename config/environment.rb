ec46c5d4df1d9a6de0dbcbc570bff33a2621c400
Hunter Meyer <huntermeyer@live.com>
2014-10-03 23:53:25 -0400

    Add mail config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Mantragram::Application.initialize!
+
+Mail.defaults do
+  delivery_method :smtp,
+  {
+    :address => 'smtp.zoho.com',
+    :port => 465,
+    :domain => 'jrdevjobs.com',
+    :user_name => ENV['MANTRA_ZOHO_UN'],
+    :password => ENV['MANTRA_ZOHO_PW'],
+    :authentication => 'login',
+    :ssl => true,
+    :tls => true,
+    :enable_starttls_auto => true
+  }
+end
