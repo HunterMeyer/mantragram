cf7b9dc52489837bb1040dec4930a4b7fc795be2
Hunter Meyer <huntermeyer@live.com>
2014-10-04 03:05:04 -0400

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :mantras
-
-  validates :mobile_number, numericality: { only_integer: true }, presence: true, uniqueness: true
+  before_validation :sanitize_mobile_number
+  before_create :activate
+  before_create :generate_reference
+  validates :mobile_number, numericality: { only_integer: true }, presence: true, uniqueness: true, length: { is: 10 }
   validates :email, uniqueness: { case_sensitive: false }
   validates :smtp_address, uniqueness: { case_sensitive: false }
+  validates :carrier, presence: true
 
   def email_required?
     false
@@ -18,6 +21,20 @@ class User < ActiveRecord::Base
     else
       where(conditions).first
     end
+  end
+
+  private
+
+  def sanitize_mobile_number
+    self.mobile_number = mobile_number.gsub(/[^0-9]/, '') if attribute_present?('mobile_number')
+  end
+
+  def activate
+    self.status = 'Active'
+  end
+
+  def generate_reference
+    self.reference = "#{DateTime.now.to_i}#{SecureRandom.hex(3)}"
   end
 
 end
