4fe5816a7ffe4a497bfadb75461bdc0ff8e6517b
Hunter Meyer <huntermeyer@live.com>
2014-10-04 07:18:46 -0400

    MVP

--- app/models/mantra.rb
+++ app/models/mantra.rb
@@ -3,7 +3,7 @@ class Mantra < ActiveRecord::Base
   has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/gif', 'image/png']
   validates_attachment_size :image, less_than: 5.megabytes
-
+  scope :active, -> { where(status: 'Active') }
 
   def deliver
     self.image.present? ? UserMailer.mantra_with_image(self.user, self) : UserMailer.mantra_with_text(self.user, self)
