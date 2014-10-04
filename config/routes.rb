a958ea9fa0a651f9699c124fdca5853d7e614819
Hunter Meyer <huntermeyer@live.com>
2014-10-04 00:44:36 -0400

    Add routes for users and mantras

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Mantragram::Application.routes.draw do
   devise_for :users
   resources :users
+  resources :mantras
 
-  root 'static_pages#index'
+  root 'static_pages#home'
+
+  match '/remove_image/:id', to: 'mantras#remove_image', via: 'post', as: 'remove_image'
+
+  match '/queue' => DelayedJobWeb, anchor: false, via: [:get, :post]
 
 end
