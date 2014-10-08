69e3f0936cba34f4b3b94b161ccbc399e0db9a02
Hunter Meyer <huntermeyer@live.com>
2014-10-08 11:58:37 -0400

    Retain original image size

--- app/models/mantra.rb
+++ app/models/mantra.rb
@@ -1,6 +1,6 @@
 class Mantra < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, styles: { large: '600x600>', medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :image
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/gif', 'image/png']
   validates_attachment_size :image, less_than: 5.megabytes
   scope :active, -> { where(status: 'Active') }
