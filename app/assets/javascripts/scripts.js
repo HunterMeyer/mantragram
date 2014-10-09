df326f6bbe5b8ff341ae9ee821f871463d99f866
Hunter Meyer <huntermeyer@live.com>
2014-10-09 18:55:34 -0400

    Change password form

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   $('.best_in_place').best_in_place();
+
+  $('#change-password').click(function() {
+    $('#edit-password-form').show();
+  });
+
+  $('#cancel-password').click(function() {
+    $('#edit-password-form').hide();
+  })
+
+
+
 });
