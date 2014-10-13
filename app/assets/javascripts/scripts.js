eacac226e72023547ae79b9d3d9d0f840a5f6532
Hunter Meyer <huntermeyer@live.com>
2014-10-12 21:32:09 -0400

    Include option to reset individual style values

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,19 +2,12 @@ $(document).ready(function() {
 
   $('.mini-color input[type=text]').minicolors();
 
-  $('.show-form').click(function() {
-    $(this).next('.row').toggleClass('hidden');
-  });
+  // Toggle forms
+  $('.show-form').click(function() { $(this).next('.row').toggleClass('hidden'); });
+  $('.cancel-form').click(function() { $(this).parent().parent().parent('.row').toggleClass('hidden'); });
+  $('.send-now').click(function() { $(this).children('i').toggleClass('hidden'); });
 
-  $('.cancel-form').click(function() {
-    $(this).parent().parent().parent('.row').toggleClass('hidden');
-  });
-
-  $('.send-now').click(function() {
-    $(this).children('i').toggleClass('hidden');
-  });
-
-  // Live updating stylesheet
+  // Live update stylesheet
   var navbar =          $('#style_navbar');
   var bg =              $('#style_background');
   var heading =         $('#style_heading');
@@ -32,6 +25,14 @@ $(document).ready(function() {
   mant_send.change(function()       { $('.send-now').css('color', $(this).val()) });
   mant_delete.change(function()     { $('.erase').css('color', $(this).val()) });
   button.change(function()          { $('#create-mantra').attr('class', 'btn btn-' + $(this).find(':selected').val()) });
+
+  // Reset style inputs
+  $('.form-group').hover(function() { $(this).children('.reset-input').children('span').toggleClass('hidden'); });
+  $('.reset-input').click(function() {
+    input = $(this).prev().children().children('input');
+    input.val(input.data('original')).change();
+    input.next().children('span').css('background-color', input.data('original'));
+  });
 
 });
 
